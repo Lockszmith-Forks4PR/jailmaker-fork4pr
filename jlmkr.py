8731de6e3bc5e7c1c5ecc3304855c696cd32027d
Lockszmith <code@lksz.me>
2024-06-27 18:18:09 -0400

    Added passing arguments to log and status
    This allows automated tests to run smoother.
    
    Currently there are 2 tests that block the console for input:
    * edit
    * shell

--- jlmkr.py
+++ jlmkr.py
@@ -490,17 +490,18 @@ def exec_jail(jail_name, cmd):
     ).returncode
 
 
-def status_jail(jail_name, args):
+def status_jail(jail_name, cmd_args):
     """
     Show the status of the systemd service wrapping the jail with given name.
     """
     # Alternatively `machinectl status jail_name` could be used
+    if not cmd_args: cmd_args = []
     return subprocess.run(
-        ["systemctl", "status", f"{SHORTNAME}-{jail_name}", *args]
+        ["systemctl", "status", f"{SHORTNAME}-{jail_name}", *cmd_args, ]
     ).returncode
 
 
-def log_jail(jail_name, args):
+def log_jail(jail_name, cmd_args):
     """
     Show the log file of the jail with given name.
     """
@@ -1887,6 +1888,13 @@ def main():
 
     for cmd in ["edit", "exec", "log", "remove", "restart", "start", "status", "stop"]:
         commands[cmd].add_argument("jail_name", help="name of the jail")
+
+    for cmd in ["log", "status"]:
+        commands[cmd].add_argument(
+            "cmd_args",
+            nargs="*",
+            help="journalctl arguments",
+        )
 
     commands["exec"].add_argument(
         "cmd",
